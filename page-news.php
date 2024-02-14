e49cf7e76c92a3a2d365c1da4e5f1f0ce05caec5
Anthony Caffiero <acaffiero@gmail.com>
2024-02-14 13:06:30 -0500

    latest

--- page-news.php
+++ page-news.php
@@ -9,86 +9,29 @@
 get_header(); 
 
 $title = get_field('title');
+$section = get_field('section');
 $hero = get_field('hero');
 
 ?>
 
-    <section id="sub-page-hero" style="background-image:url(<?php echo $hero['url']; ?>);">
+    <section id="sub-page-hero" class="<?php echo $section; ?> news" >
       <div class="wrapper">
        
+        <div class="rip-top"></div>
+        <div class="rip-bot"></div>
+      
         <div class="inner">
-          <div><h1><?php echo $title; ?></h1></div>
+          <div class="text">
+            <h2><?php echo $section; ?></h2>
+            <h1><?php echo $title; ?></h1>
+          </div>
+          <div class="image"><img src="<?php echo $hero['url']; ?>" alt="<?php echo $hero['alt']; ?>" /></div>
         </div>
-     
+    
      </div> 
     </section>
       
-      <?php
-      $args = array( 
-          'post_type' => 'post', 
-          'posts_per_page' => 1, 
-          'order'=> 'ASC',
-          'orderby'=> 'menu_order'
-      );
-      $latestpost = new WP_Query($args);
-   
-      if ($latestpost) {          
-        while( $latestpost->have_posts() ): $latestpost->the_post(); 
-        $type = get_field('type');
-         $posttags = get_the_tags(); 
-        $quote = get_field('quote');
-        $post_date = get_the_date( 'F j, Y' ); 
-        $teaser = get_the_excerpt();
-        $url = get_field('third_party_url');
-        ?>
-     
-        <section class="two-cols news-rows no-padding-bottom news-big-row">
-          <div class="wrapper">
-            
-            <h3>Featured Story</h3>
-    
-           <div class="row featured-row">
-             <div class="col">
-               <div class="news-item ">
-                 
-                 <?php if ($url) { ?>
-                                   <a href="<?php echo $url; ?>" target="_blank"><?php echo get_the_post_thumbnail( $post_id, 'full' );  ?></a>
-                                   <?php }
-                                   else { ?>
-                                   <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_post_thumbnail( $post_id, 'full' );  ?></a>
-                 <?php } ?>
-                 
-                
-               </div>
-             </div>
-             <div class="col">
-               <div class="news-item big">
-                   <p class="date"><?php echo $post_date;?> | <span style="text-transform:uppercase;"><?php if ($posttags) {
-                   foreach($posttags as $tag) {
-    	             echo $tag->name . '<span>, </span> '; 
-	               }
-              } ?></span></p>
-                   <?php if ($url) { ?>
-                                     <a href="<?php echo $url; ?>" target="_blank"><?php the_title(); ?></a>
-                                     <?php }
-                                     else { ?>
-                                     <a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a>
-                   <?php } ?>
-                   
-                   <div class="teaser"><?php echo $teaser; ?></div>
-               </div>
-             </div>
-           </div>
-    
-          </div>
-        </section>
-     
-        <?php  
-        endwhile; /* end secondary loop */
-     
-      }
-     ?>
-  
+ 
   
 
 
@@ -114,7 +57,7 @@ $hero = get_field('hero');
               repeater="template_5" 
               post_type="post" 
               posts_per_page="9" 
-              offset="1" 
+              offset="0" 
               scroll="false"
               id="newslist"
               filter="news"
